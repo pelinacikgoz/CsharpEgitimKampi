863b6fe24f711c094942f21cc7d85053b7a63829
Pelin Açıkgöz <pelinacikgoz@marun.edu.tr>
2026-06-23 22:10:30 +0300

    4. ve 5. ders döngüler

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For döngüsü
+            //Döngüler
+
+            //For(x;y;z) 
+            //x: başlangıç değeri
+            //y: bitiş değeri
+            //z:artış veya azalış
+
+
+            //int i;
+
+            //for (i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("C# eğitim kampı");
+            //}
+
+            //  for(int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i <= 50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // i +=3 dediğin zaman step 3 gibi düşün 
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adedi giriniz: ");
+            //int finishvalue=int.Parse(Console.ReadLine());  
+
+            //for(int i=1; i<=finishvalue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+
+            //kullanıcının istediği değere kadar yazdırılır
+
+            #endregion
+
+            #region For Döngüsü İle Karar Yapıları
+
+            //for(int i=0; i<=100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+            //int totalvalue = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    totalvalue += i;
+
+            //}
+            //Console.WriteLine(totalvalue);
+
+            //+= işareti i nin istenilen değere kadar toplanmasını ifade eder
+            //totalvalue değerinin ilk değerini mutlaka loop dışarısına yaz
+
+            //int totalvalue = 0;
+            //for (int i = 0; i <= 20; i += 2)
+            //{
+            //    totalvalue += i;
+            //}
+            //Console.WriteLine(totalvalue);
+
+
+            //int totalvalue = 0;
+            //for(int i=1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalvalue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine("-----------------");
+            //Console.WriteLine(totalvalue);
+
+
+            //int count =0;
+
+            //for(int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //Console.WriteLine("------------------");
+            //Console.WriteLine(count);
+
+            //count++ olarak yazdık ki i değerini her bulduğunda count değeri de artsın
+
+            //int bacterium = 1;
+
+            //for(int i = 1; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + " .Saat Sonunda: " + bacterium);
+            //}
+
+            //i değerini saat olarak düşündük ve for döngüsü ile 24 saat sonunda oluşan bakteri sayısı bulundu
+
+
+
+            #endregion
+
+            #region While Döngüsü
+
+            //While(şart)
+            //{
+            //işlemler
+            //}
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}
+
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+
+            //int i = 1;
+            //int totalvalue = 0;
+            //while (i <= 10)
+            //{
+            //    totalvalue += i;
+            //    i++;
+
+            //}
+            //Console.WriteLine(totalvalue);
+
+            #endregion
+
+            #region Örnek Sınav Sorusu
+
+            //klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız
+
+            //Console.Write("Lütfen sayıyı giriniz: ");
+            //int number=int.Parse(Console.ReadLine());
+
+            //int ones, tens, hundreds;
+            //int sum;
+
+            //ones = number % 10;
+            //hundreds = number / 100; //4.56 --> integer dediğimiz için direkt 4 değerini verir
+            //tens = (number % 100) / 10;
+
+            //Console.WriteLine(ones + "-" + tens + "-" + hundreds);
+            //sum=ones+tens+hundreds;
+            //Console.WriteLine(sum);
+
+
+            #endregion
+
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ 05_LoopswithStars/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopswithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region alt alta 10 tane yıldız oluşturma
+
+            //for(int i=1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+
+            #region yan yana 10 tane yıldız oluşturma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region alt alta 10 tane yıldız oluşturma ve her satırda 10 tane yıldız
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+            #endregion
+
+
+            #region dik üçgen oluşturma
+
+            //for(int i = 1; i <= 5; i++)
+            //{
+            //    for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region ters dik üçgen oluşturma
+
+            //for (int i = 5; i>= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            #endregion
+
+            #region Baklava şekli oluşturma
+            //2 şekilde yazıldı
+
+            //for(int i=1; i <= 5; i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for(int k = 4; k >= 1; k--)
+            //{
+            //    for(int m=1;m<=k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //üst kısım
+            //int n= 5;
+            //for(int i=1;i<=n;i++)
+            //{
+            //    for(int j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int k=1; k <= (2 * i - 1); k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+
+            //alt kısım
+            //for(int i= n - 1; i >= 1; i--)
+            //{
+            //    for(int j=1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int k=1;k<=(2* i - 1); k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+            #endregion
+
+
+            #region piramit oluşturma
+
+
+            //int n = 5;
+            //for(int i = 1; i <= n; i++)
+            //{
+            //    //boşluklar için
+            //    for(int j = n - i; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    //yıldızlar için
+            //    for(int k=1;k<=2*i-1;k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+            //baklava diliminin üst kısmı gibi de yazılabilir
+            //int n = 5;
+            //for(int i=1;i<=n;i++)
+            //{
+            //    for(int j=1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(int k=1; k <= (2 * i - 1); k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+            #endregion
+
+
+            #region ters piramit oluşturma
+
+           // int n = 5;
+            
+
+           //for(int i=n; i >= 1; i--)
+           // {
+           //     for(int j = n - i; j > 0; j--)
+           //     {
+           //         Console.Write(" ");
+           //     }
+           //     for(int k = 1; k <= 2 * i - 1; k++)
+           //     {
+           //         Console.Write("*");
+           //     }
+           //     Console.WriteLine();
+           // }
+
+
+            #endregion
+
+
+
+            Console.Read();
+
+
+
+        }
+    }
+}
